dc8249560116bf8edc31430f4956da47121f19d1
Slacker2519 <tienduongeng2001@gmail.com>
2025-12-10 20:04:47 +0000

    + Add detail card for both mobile and pc layout * Need to fix detail card animation

--- p-allery/src/FocusedImage.jsx
+++ p-allery/src/FocusedImage.jsx
@@ -6,13 +6,12 @@ const FocusedImage = (props) => {
     const [openDetail, setOpenDetail] = useState(false);
 
     return (
-        <div className="flex justfify-center items-center z-30">
-            <div className="relative flex justfify-center items-center bg-light p-2 md:p-3
-                rounded-lg shadow-lg shadow-light/50 animate-zoomIn">
+        <div className="focused-image">
+            <div className="focused-image-container animate-zoomIn">
                 <img
                     src={image.imageUrl}
                     alt={image.description}
-                    className="max-w-[80vw] max-h-[80vh] object-contain"
+                    className="max-w-[80vw] md:max-w-[55vw] max-h-[80vh] object-contain"
                 />
                 <button
                     className="absolute top-2 md:top-4 right-1 md:right-3 rounded-full"
@@ -21,15 +20,17 @@ const FocusedImage = (props) => {
                     <i className="fa-solid fa-ellipsis-vertical text-xl md:text-2xl text-green-400"></i>
                 </button>
             </div>
-            <div>
-                <ImageDetailCard
-                    isOpen={openDetail}
-                    onClose={() => setOpenDetail(false)}
-                    tags={image.tag}
-                    author={image.author}
-                    description={image.description}
-                />
-            </div>
+            {openDetail && (
+                <div>
+                    <ImageDetailCard
+                        isOpen={openDetail}
+                        onClose={() => setOpenDetail(false)}
+                        tags={image.tag}
+                        author={image.author}
+                        description={image.description}
+                    />
+                </div>
+            )}
         </div>
     );
 }
--- p-allery/src/ImageDetailCard.jsx
+++ p-allery/src/ImageDetailCard.jsx
@@ -5,18 +5,23 @@ const ImageDetailCard = (props) => {
     const { isOpen, onClose, tags, author, description } = props;
 
     return (
-        <div>
+        <div className="h-full">
             {isOpen && (
-                <div>
+                <div className="md:hidden">
                     <Overlay zIndex="z-40" onClick={onClose} />
                 </div>
             )}
             <aside className={`
-                fixed bottom-0 fixed left-1/2 -translate-x-1/2 flex flex-col mb-3
-                w-5/6 h-2/4 bg-light dark:bg-dark rounded-lg overflow-y-auto z-50
-                p-4 transform-animation
-                ${isOpen ? '-translate-y-0' : 'translate-y-[120%]'}
+                detail-card transform-animation
+                ${isOpen ? 
+                'translate-y-0 md:translate-x-[5%]' : 
+                'translate-y-[120%] md:translate-y-0 md:-translate-x-[120%]'}
             `}>
+                <button
+                    className="hidden self-end md:inline rounded-full mb-3"
+                    onClick={onClose}>
+                    <i className="fa-solid fa-xmark"></i>
+                </button>
                 <div className="mb-3">Tags:
                     {tags.map((tag, index) =>
                         <TagCard key={index} children={tag} />
